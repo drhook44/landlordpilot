5685e24911f83bb8f78895b14945f00e463e7fc4
drhook44 <samjonesim@gmail.com>
2026-08-13 22:08:49 -0400

    Fix next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ["images.unsplash.com"],
   },
-  basePath: '',
-  assetPrefix: '',
 };
 
 module.exports = nextConfig;
